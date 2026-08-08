1a44fe8c2cc933fdc169ffad8c5f7d8054782e3a
Louis <louis.dubois@epitech.eu>
2026-08-08 19:55:43 +0200

    ADD: create connection

--- HabitLogger.Console/Program.cs
+++ HabitLogger.Console/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HabitLoggerLibrary;
+
+namespace HabitLoggerProgram;
+
+class Program
+{
+    static void Main()
+    {
+        HabitLogger.CreateConnection();
+    }
+}
--- HabitLoggerLibrary/HabitLoggerLibrary.cs
+++ HabitLoggerLibrary/HabitLoggerLibrary.cs
@@ -5,5 +5,32 @@ namespace HabitLoggerLibrary;
 
 public class HabitLogger
 {
+    private static void ExecuteQuery(string query)
+    {
+        using var connection = new SqliteConnection("Data Source=habitlogger.db");
+        SqliteCommand sqlCommand = new(query, connection);
     
+        try
+        {
+            connection.Open();
+            sqlCommand.ExecuteNonQuery();
+            Console.WriteLine("DataBase is Created Successfully");
+        } catch (SqliteException e)
+        {
+            Console.WriteLine($"Sqlite Error: {e.Message}");
+        }
+    }
+    public static void CreateConnection()
+    {
+        var createDB = """
+            CREATE TABLE IF NOT EXISTS HabitLog
+            (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                quantity INT NOT NULL,
+                date TEXT NOT NULL
+            )
+        """;
+        ExecuteQuery(createDB);
+    }
+
 }
